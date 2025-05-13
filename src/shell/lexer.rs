eb57a5cf345226ffb38643fc58581498c71ea764
Shlomo Ballew <gavib18@gmail.com>
2025-05-13 14:53:24 -0400

    added lexer

---
+++ src/shell/lexer.rs
@@ -0,0 +1,125 @@
+use std::collections::HashMap;
+
+#[derive(Clone)]
+enum TokenType {
+    //literal types
+    Number,
+    Identifier,
+
+    //keywords
+    Var,
+    Function,
+
+    //grouping operators
+    Equals,
+    OpenParen,
+    CloseParen,
+    OpenBrace,
+    CloseBrace,
+    OpenBracket,
+    CloseBracket,
+    BinaryOperator,
+    EOF //end of file
+}
+
+struct Token {
+    token_value: String,
+    token_type: TokenType
+}
+
+impl Token {
+    pub fn new(token_value: &str, token_type: TokenType) -> Self {
+        Token {
+            token_value: token_value.to_string(),
+            token_type,
+        }
+    }
+}
+
+fn is_skippable(c: &char) -> bool {
+    *c == ' ' || *c == '\n' || *c == '\t'
+}
+
+fn make_keywords_map() -> HashMap<String, TokenType> {
+    let mut keywords = HashMap::new();
+    keywords.insert(String::from("var"), TokenType::Var);
+    keywords.insert(String::from("function"), TokenType::Function);
+    keywords
+}
+
+fn tokenize(source_code: String) -> Vec<Token> {
+    let mut tokens: Vec<Token> = Vec::new();
+    let mut src = source_code.chars().peekable();
+    let keywords = make_keywords_map();
+    while let Some(current) = src.peek() {
+        match current {
+            '(' => {
+                tokens.push(Token::new("(", TokenType::OpenParen));
+                src.next();
+            },
+            ')' => {
+                tokens.push(Token::new(")", TokenType::CloseParen));
+                src.next();
+            },
+            '[' => {
+                tokens.push(Token::new("[", TokenType::OpenBracket));
+                src.next();
+            },
+            ']' => {
+                tokens.push(Token::new("]", TokenType::CloseBracket));
+                src.next();
+            },
+            '{' => {
+                tokens.push(Token::new("{", TokenType::OpenBrace));
+                src.next();
+            },
+            '}' => {
+                tokens.push(Token::new("}", TokenType::CloseBrace));
+                src.next();
+            },
+            '+' | '-' | '*' | '/' | '%' => {
+                tokens.push(Token::new(&current.to_string(), TokenType::BinaryOperator));
+                src.next();
+            },
+            '=' => {
+                tokens.push(Token::new("=", TokenType::Equals));
+                src.next();
+            },
+            _ if current.is_digit(10) => {
+                let mut num = String::new();
+                while let Some(next) = src.peek() {
+                    if next.is_digit(10) {
+                        num.push(*next);
+                        src.next();
+                    } else {
+                        break;
+                    }
+                }
+                tokens.push(Token::new(&num, TokenType::Number));
+            },
+            _ if current.is_alphabetic() => {
+                let mut ident = String::new();
+                while let Some(next) = src.peek() {
+                    if next.is_alphabetic() {
+                        ident.push(*next);
+                        src.next();
+                    } else {
+                        break;
+                    }
+                }
+                if let Some(token_type) = keywords.get(&ident) {
+                    tokens.push(Token::new(&ident, token_type.clone()));
+                } else {
+                    tokens.push(Token::new(&ident, TokenType::Identifier))
+                }
+            },
+            _ if is_skippable(current) => {src.next();}
+            x => {
+                println!("Unrecognized character found: {}", x);
+                std::process::exit(1);
+            }
+        }
+    }
+    tokens.push(Token::new("EndOfFile", TokenType::EOF));
+    tokens
+}
